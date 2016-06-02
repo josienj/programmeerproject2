b30f4c5f331e6f14c043ffb31516f4566821cb20
josienj <josienjansen1@gmail.com>
2016-06-03 01:23:49 +0200

    xml done

--- app/src/main/java/com/example/josien/programmeerproject2/CheckIn.java
+++ app/src/main/java/com/example/josien/programmeerproject2/CheckIn.java
@@ -1,13 +1,95 @@
 package com.example.josien.programmeerproject2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class CheckIn extends AppCompatActivity {
+import com.facebook.FacebookSdk;
+
+public class CheckIn extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkin);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_checkin) {
+            Intent checkin = new Intent(this, MainActivity.class);
+            checkin.putExtra("Checkin", 500);
+            startActivity(checkin);
+
+        } else if (id == R.id.nav_friendscheckin) {
+            Intent friendscheckin = new Intent(this, Friends_checkin.class);
+            friendscheckin.putExtra("friendscheckin", 500);
+            startActivity(friendscheckin);
+        }  else if (id == R.id.nav_instellingen) {
+            Intent instellingen = new Intent(this, Instellingen.class);
+            instellingen.putExtra("instellingen", 500);
+            startActivity(instellingen);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void check_in_train(View view){
+        Toast.makeText(this, "Je bent ingecheckt in deze trein!", Toast.LENGTH_SHORT).show();
+        Intent friends = new Intent(this, Friends_checkin.class);
+        friends.putExtra("friends", 500);
+        startActivity(friends);
+    }
+
+
 }
--- app/src/main/java/com/example/josien/programmeerproject2/Friends_checkin.java
+++ app/src/main/java/com/example/josien/programmeerproject2/Friends_checkin.java
@@ -23,7 +23,7 @@ public class Friends_checkin extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_friendscheckin);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
